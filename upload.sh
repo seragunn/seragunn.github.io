37baf784eebccf35219ad9485b8dcb31e7289912
Trevor Gunn <tgunn@live.ca>
2023-10-03 12:39:18 -0400

    no need for upload.sh

--- upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-scp index.html tgunn8@ssh.math.gatech.edu:/home/tgunn8/public_html
-scp style.css  tgunn8@ssh.math.gatech.edu:/home/tgunn8/public_html
-ssh tgunn8@ssh.math.gatech.edu publish
